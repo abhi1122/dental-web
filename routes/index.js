9cf383720fe23fb22e57905183c77ed5655f7cc6
Sharma, Abhishek <asharma2@conversantmedia.com>
2021-09-13 20:42:13 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+function changeLang(req) {
+    const data = require('../lang/eng.json');
+    return data;
+}
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        ...changeLang(req)
+    });
+});
+
+router.get('/treatment-facilities', function (req, res, next) {
+    res.render('treatment', {
+        ...changeLang(req)
+    });
+});
+
+router.get('/about-us', function (req, res, next) {
+    res.render('about-us', {
+        ...changeLang(req)
+    });
+});
+
+router.get('/gallery', function (req, res, next) {
+    res.render('gallery', {
+        ...changeLang(req)
+    });
+});
+
+router.get('/contact-us', function (req, res, next) {
+    res.render('contact-us', {
+        ...changeLang(req)
+    });
+});
+
+module.exports = router;
